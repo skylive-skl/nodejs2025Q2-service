f71b5582a592b8bfceab48cf82a3ce44ac35ab17
ismet <kadyrovismet1999@gmail.com>
2025-06-01 01:42:22 +0500

    fix: fix swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { SwaggerModule } from '@nestjs/swagger';
+import { parse } from 'yaml';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
+  const document = readFileSync(join(__dirname, '..', 'doc/api.yaml'), 'utf8');
+  SwaggerModule.setup('/doc', app, parse(document));
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`API documentation: http://localhost:${PORT}/doc`);
+  });
 }
 bootstrap();
